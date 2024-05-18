1b4af81dbad7a339d399f125456bcc25c3aaf083
Hvk26 <hvk8789@gmail.com>
2024-05-18 21:21:12 +0530

    added different objects and used props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import List from './List.jsx'
 
 function App() {
+
+    const fruits = [{id: 1, name:"Apple", calories: 96}, {id: 2, name:"Banana", calories: 107}, {id: 3, name:"Mango", calories: 125}, {id: 4, name:"Pineapple", calories: 57}, {id: 5, name:"Grapes", calories: 75}]
+    const vegetables = [{id: 1, name:"Radish", calories: 51}, {id: 2, name:"Brinjal", calories: 115}, {id: 3, name:"Potato", calories: 139}, {id: 4, name:"Carrot", calories: 77}, {id: 5, name:"Corn", calories: 56}]
+
  return(
     <>
-        <List/>
+        <List items={fruits} category="Fruits"/>
+        <List items={vegetables} category="Vegetables"/>
     </>
  );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,14 +1,16 @@
-function List(){
-    const fruits = [{id: 1, name:"Apple", calories: 96}, {id: 2, name:"Banana", calories: 107}, {id: 3, name:"Mango", calories: 125}, {id: 4, name:"Pineapple", calories: 57}, {id: 5, name:"Grapes", calories: 75}]
+function List(props){
+    
+    const itemsList = props.items
+    const category = props.category
 
-    fruits.sort((a,b)=> a.name.localeCompare(b.name));
-
-    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
-
-    const listItems = lowCalFruits.map(fruit => <li key={fruit.id}>{fruit.name}: <b>{fruit.calories}</b></li>)
+    const listItems = itemsList.map(Items => <li key={Items.id}>{Items.name}: <b>{Items.calories}</b></li>)
 
     return(
-        <ul>{listItems}</ul>
+        <>
+            <h2>{category}</h2>
+            <ul>{listItems}</ul>
+        </>
+        
     );
 }
 
