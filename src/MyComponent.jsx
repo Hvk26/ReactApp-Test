2277fed3ea5e71fc2ef201019f77ecd5358a0324
Hvk26 <hvk8789@gmail.com>
2024-05-20 10:34:28 +0530

    added incremental function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,15 +2,23 @@ import React, {useState} from 'react';
 
 function MyComponent(){
 
-    const[name, setName] = useState();
+    const[name, setName] = useState("Guest");
+    const[age, setAge] = useState(0);
 
     const updateName = () =>{
         setName("HVK");
     }
 
+    const increamentAge = () =>{
+        setAge(age + 1);
+    }
+
     return(<div>
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={increamentAge}>Set Name</button>
     </div>);
 
 
