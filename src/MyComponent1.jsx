5642fb9b7a1e7ebfef5221200226dfc1da7e2c43
Hvk26 <hvk8789@gmail.com>
2024-05-22 16:19:50 +0530

    adding window change eventlistener

--- src/MyComponent1.jsx
+++ src/MyComponent1.jsx
@@ -2,30 +2,41 @@ import React, {useState, useEffect} from 'react';
 
 function MyComponent1(){
 
-    const[count, setCount] = useState(0);
-    const[color, setColor] = useState();
+    // const[count, setCount] = useState(0);
+    // const[color, setColor] = useState();
+       const[height, setHeight] = useState(window.innerHeight);
+       const[width, setWidth] = useState(window.innerWidth);
 
 
+    // useEffect(function changeTitle(){
+    //     document.getElementById('siteName').innerText = `Count: ${count} ${color}`
+    // }, [count,color]);
+       useEffect(()=>{
+        window.addEventListener("resize", sizeHandler);
+       })
 
-    useEffect(function changeTitle(){
-        document.getElementById('siteName').innerText = `Count: ${count} ${color}`
-    }, [count,color]);
-
-    const addCount =()=>{
-        setCount(c=> c +1 );
-    }
-    const substractCount =()=>{
-        setCount(c=> c -1 );
-    }
-    const changeColor =()=>{
-        setColor(c=> c=== "green" ? "red": "green");
-    }
+    // const addCount =()=>{
+    //     setCount(c=> c +1 );
+    // }
+    // const substractCount =()=>{
+    //     setCount(c=> c -1 );
+    // }
+    // const changeColor =()=>{
+    //     setColor(c=> c=== "green" ? "red": "green");
+    // }
+       const sizeHandler =()=>{
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+       }
 
     return(<div>
-        <p style={{color: color}}>count: {count}</p>
+        {/* <p style={{color: color}}>count: {count}</p>
         <button onClick={addCount}>add</button>
         <button onClick={substractCount}>substract</button>
-        <button onClick ={changeColor}>Change color</button>      
+        <button onClick ={changeColor}>Change color</button>       */}
+        <p>window height: {height}</p>
+        <p>window width: {width}</p>
+
     </div>);
 
 }
