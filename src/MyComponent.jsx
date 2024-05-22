70486eb0ee3645d9bc24e72095110978dc3540f1
Hvk26 <hvk8789@gmail.com>
2024-05-22 15:05:56 +0530

    adding instruction comment to text-area

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -7,6 +7,7 @@ function MyComponent(){
     const [quantity, setQuantity] = useState(1);
     const [payment, setPayment] = useState("");
     const [method, setMethod] = useState("");
+    const [comment, setComment] = useState("");
 
 
     // declaring different function
@@ -21,6 +22,9 @@ function MyComponent(){
     }
     const methodHandler = ()=>{
         setMethod(event.target.value);
+    }
+    const commentHandler = ()=>{
+        setComment(event.target.value);
     }
 
     return(<div>
@@ -42,6 +46,9 @@ function MyComponent(){
         <label> <input type="radio" value="Pick Up" checked={method === "Pick Up"} onChange={methodHandler}/>Pick Up</label><br/>
         <label> <input type="radio" value="Delivery" checked={method === "Delivery"} onChange={methodHandler}/>Delivery</label>
         <p>Shipping: {method}</p>
+
+        <textarea name="comment" placeholder="add instruction" onChange ={commentHandler}></textarea>
+        <p>Instruction: {comment} </p>
     </div>);
 
 }
