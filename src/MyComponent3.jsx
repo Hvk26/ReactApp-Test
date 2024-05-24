b02ec0d2877379854200dafcff629a1d492f4170
Hvk26 <hvk8789@gmail.com>
2024-05-24 23:58:28 +0530

    adding edition to update object component

--- src/MyComponent3.jsx
+++ src/MyComponent3.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 function MyComponent3(){
 
-    const [car, setCar] = useState({year: 2024, make: "Audi", model: "Q3"})
+    const [car, setCar] = useState({year: 2024, make: "Audi", model: "Q3", edition: "limited"})
 
     const changeMake = ()=>{
         setCar(c=> ({...c, make: event.target.value}));
@@ -12,11 +12,23 @@ function MyComponent3(){
         setCar(c=> ({...c, model: event.target.value}));
     }
 
+    const changeYear = ()=>{
+        setCar(c=> ({...c, year: event.target.value}));
+    }
+
+    const changeEdition = ()=>{
+        setCar(c=> ({...c, edition: event.target.value}));
+    }
+
+
+
     return(
         <>
             <input type="text" value={car.make} onChange={changeMake} />
             <input type="text" value={car.model} onChange={changeModel} />
-            <div>Your favorite car is: {car.make} {car.model} {car.year}</div>
+            <input type="number" value={car.year} onChange={changeYear} />
+            <input type="text" value={car.edition} onChange={changeEdition} />
+            <div>Your favorite car is: {car.make} {car.model} {car.year} {car.edition} edition</div>
         </>
         
     );
